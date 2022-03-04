473a9d5436aff04b0db290c111ff715fb5820c81
seasameoil <whdwlgus12@gmail.com>
2022-03-04 21:33:01 +0900

    SecurityConfig 생성

---
+++ src/main/java/photogram/cos/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package photogram.cos.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity //해당 파일로 시큐리티 활성화
+@Configuration //IoC
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //super 삭제 - 기존 시큐리티가 가지고 있는 기능이 다 비활성화 됨.
+        http.authorizeRequests()
+                .antMatchers("/", "/user/**", "/image/**", "/subscribe/**", "/comment/**").authenticated()
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/auth/signin")
+                .defaultSuccessUrl("/");
+    }
+}
