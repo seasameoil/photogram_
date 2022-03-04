e43ed31042f1dfc5a38ebae32202bf18a028647e
seasameoil <whdwlgus12@gmail.com>
2022-03-04 21:05:27 +0900

    Initial commit

---
+++ src/test/java/photogram/cos/CosApplicationTests.java
@@ -0,0 +1,13 @@
+package photogram.cos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
