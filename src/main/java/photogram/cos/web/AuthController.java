39d897b63cc9a293be4b91d856cf0a7a427712e0
seasameoil <whdwlgus12@gmail.com>
2022-03-10 23:59:23 +0900

    회원가입 - @ResponseBody

--- src/main/java/photogram/cos/web/AuthController.java
+++ src/main/java/photogram/cos/web/AuthController.java
@@ -6,6 +6,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import photogram.cos.domain.user.User;
 import photogram.cos.service.AuthService;
 import photogram.cos.web.dto.auth.SignupDto;
@@ -31,19 +32,19 @@ public class AuthController {
     }
 
     @PostMapping("/auth/signup")
-    public String signup(@Valid SignupDto signupDto, BindingResult bindingResult) {
+    public @ResponseBody String signup(@Valid SignupDto signupDto, BindingResult bindingResult) {
 
         if(bindingResult.hasErrors()) {
             Map<String, String> errorMap = new HashMap<>();
 
             for(FieldError error : bindingResult.getFieldErrors()) {
                 errorMap.put(error.getField(), error.getDefaultMessage());
-                System.out.println("Error Message : " + error.getDefaultMessage());
             }
+            return "오류남";
+        } else {
+            User user = signupDto.toEntity();
+            authService.회원가입(user);
+            return "/auth/signin";
         }
-
-        User user = signupDto.toEntity();
-        authService.회원가입(user);
-        return "/auth/signin";
     }
 }
--- src/main/java/photogram/cos/web/dto/auth/SignupDto.java
+++ src/main/java/photogram/cos/web/dto/auth/SignupDto.java
@@ -3,13 +3,13 @@ package photogram.cos.web.dto.auth;
 import lombok.Data;
 import photogram.cos.domain.user.User;
 
-import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Data
 public class SignupDto {
 
-    @Max(20)
+    @Size(min = 2, max = 20) @NotBlank
     private String username;
     @NotBlank
     private String password;
